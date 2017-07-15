49831e7d2816b35aba55cfd6769c796ebe18907e
Ben Whittle <benwhittle31@gmail.com>
2017-07-14 20:37:59 -0700

    Adding script to split image data into train/test/validation sets

--- definitions.py
+++ definitions.py
@@ -3,3 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 data_dir = basedir + '/data'
 dml_dir = data_dir + '/dml'
+image_dir = data_dir + '/images'
+tmp_dir = basedir + '/tmp'
