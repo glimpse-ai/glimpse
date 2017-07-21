f8d51dbcbdfcea07c6da72e5f1473a58cc8bcb59
Ben Whittle <benwhittle31@gmail.com>
2017-07-21 14:03:38 -0700

    cleaning up setup.py

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ import os
 from helpers.definitions import data_dir
 
 
-glimpse_dir = os.environ.get('S3_GLIMPSE_DIR').rstrip('/')
+s3_glimpse_dir = os.environ.get('S3_GLIMPSE_DIR').rstrip('/')
 
-
+# Create data dir if not already there
 if not os.path.exists(data_dir):
   os.mkdir(data_dir)
 
+# Pull the following files from S3 bucket and move them into the data dir
 for f in ['train.pkl', 'val.pkl', 'test.pkl', 'vocab.json']:
   dest_path = '{}/{}'.format(data_dir, f)
   
   if os.path.exists(dest_path):
     continue
   
-  os.system('wget {}/{}'.format(glimpse_dir, f))
+  os.system('wget {}/{}'.format(s3_glimpse_dir, f))
   os.system('mv {} {}'.format(f, dest_path))
