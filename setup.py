782803e043e213a5a7378e9fd974c637ea93323b
Ben Whittle <benwhittle31@gmail.com>
2017-07-25 15:19:06 -0700

    adding the fetching of vocab.json to setup.py

--- setup.py
+++ setup.py
@@ -30,4 +30,10 @@ if __name__ == '__main__':
   if not os.path.exists(data_dir):
     os.mkdir(data_dir)
   
+  # Pull images and dml
   [pull_s3_data_dir(f) for f in ['images', 'dml']]
+  
+  # Pull vocab.json
+  vocab_fname = 'vocab.json'
+  os.system('wget {}/{}'.format(s3_glimpse_dir, vocab_fname))
+  os.system('mv {} {}/{}'.format(vocab_fname, data_dir, vocab_fname))
