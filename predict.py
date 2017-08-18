a89d238dc4451d53898623208d84ef64d5b93052
Ben Whittle <benwhittle31@gmail.com>
2017-08-17 18:29:56 -0700

    removing redundant data extraction from predict.py

--- predict.py
+++ predict.py
@@ -1,10 +1,10 @@
-from glimpse.helpers import dataset
 from glimpse.trainer import Trainer
 
 
 if __name__ == '__main__':
-  images, labels, label_lens = dataset.test()
+  trainer = Trainer(feed_previous=True)
+
+  images = trainer.X_test
   image_batch = images[::4][:4]
 
-  trainer = Trainer(feed_previous=True)
   trainer.predict(image_batch)
