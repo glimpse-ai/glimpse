fb211b2eeb4872174895b2a9ef6567b78d809392
gabriel <gmaher2@hotmail.com>
2017-08-28 18:35:33 -0700

    prototyping update

--- glimpse/helpers/definitions.py
+++ glimpse/helpers/definitions.py
@@ -1,6 +1,7 @@
 import os
 
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+print basedir
 data_dir = basedir + '/data'
 dml_dir = data_dir + '/dml'
 image_dir = data_dir + '/images'
